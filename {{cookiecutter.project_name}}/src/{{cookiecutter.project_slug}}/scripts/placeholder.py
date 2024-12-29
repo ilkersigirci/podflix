4988cb7a83ad0cf2b173cc9dafdaba01370a9f61
İlker SIĞIRCI <39417966+ilkersigirci@users.noreply.github.com>
2024-12-30 01:45:39 +0300

    Initial commit

---
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/scripts/placeholder.py
@@ -0,0 +1,12 @@
+"""Useful python cli scripts.
+
+Currently, `uv` not support this feature.
+"""
+
+
+def placeholder_script():
+    """Script that can be run in the command line.
+
+    More detail: https://setuptools.pypa.io/en/latest/userguide/entry_point.html
+    """
+    print("This is a placeholder script that can be run as `entrypoint`")  # noqa: T201
